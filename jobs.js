e253dbbc2e74bdc3661c5ded1df6b51d168e62bb
Dillon McKenna <dmckenna05@gmail.com>
2022-04-24 16:47:28 -1000

    Change getDate to UTC

--- jobs.js
+++ jobs.js
@@ -4,8 +4,6 @@ const https = require("https");
 const CronJob = require("cron").CronJob;
 
 
-
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -14,7 +12,8 @@ function getRandomInt(min, max) {
 
 const getDate = function() {
   const today = new Date()
-  return today.toLocaleDateString();
+  const options = {year:'numeric', month:'numeric', day:'numeric', timeZone: 'UTC'};
+  return today.toLocaleDateString('en-US', options);
 };
 
 const sayingSchema = new mongoose.Schema({
@@ -63,6 +62,103 @@ const Highlight = mongoose.model("highlight", highlightSchema);
 
 
 // ------------------- cron jobs -----------------------------//
+
+//
+// const newSaying = new CronJob('5 39 2 * * *', function() {
+//   https.get("https://api.quotable.io/random", function(response) {
+//     response.on("data", function(data) {
+//       const quote = JSON.parse(data);
+//       const saying = new Saying({
+//         name: quote.author,
+//         quote: quote.content,
+//         date: getDate()
+//       });
+//       saying.save();
+//     });
+//   });
+// }, null, true, 'UTC');
+//
+// const newPoem = new CronJob('5 39 2 * * *', function() {
+//   const lineCount = getRandomInt(8, 100);
+//   axios.get(`https://poetrydb.org/random,linecount/1;${lineCount}`)
+//     .then(response => {
+//       const poemData = response.data[0];
+//       const poem = new Poem({
+//         author: poemData.author,
+//         title: poemData.title,
+//         content: poemData.lines,
+//         date: getDate()
+//       });
+//       poem.save();
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }, null, true, 'UTC');
+//
+// const newJoke = new CronJob('5 39 2 * * *', function() {
+// axios("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit&type=twopart")
+//   .then(response => {
+//     const joke = new Joke({
+//       setup: response.data.setup,
+//       punchline: response.data.delivery,
+//       date: getDate()
+//     });
+//     joke.save();
+//   })
+// }, null, true, 'UTC');
+//
+// const newRecipe = new CronJob('5  2 * * *', function() {
+// axios.get("https://api.spoonacular.com/recipes/random?apiKey=690ded73385c4b61a0d2217384d64b16&tags=main%20course", {
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//   .then(response => {
+//     const newData = response.data;
+//     const retrievedRecipe = newData.recipes[0];
+//     const ingredients = retrievedRecipe.extendedIngredients;
+//     const sortedIngredients = [];
+//     ingredients.forEach(ingredient => {
+//       sortedIngredients.push(ingredient.original);
+//     });
+//     const recipe = new Recipe({
+//       dishName: retrievedRecipe.title,
+//       time: retrievedRecipe.readyInMinutes,
+//       servings: retrievedRecipe.servings,
+//       ingredients: sortedIngredients,
+//       instructions: retrievedRecipe.instructions,
+//       source: retrievedRecipe.sourceUrl,
+//       imageURL: retrievedRecipe.image,
+//       date: getDate()
+//     })
+//     recipe.save();
+//     });
+// }, null, true, 'UTC');
+//
+// const newHighlight = new CronJob('5 21 2 * * *', function() {
+//   axios.get("https://www.scorebat.com/video-api/v3/feed/?token=MTcxODhfMTY0OTgwMzQ4MF9iYWJiMDQ4ZDQzYWI3ZjcxZjdmZWUxY2NiNjVkMTcwMTAzZjcwYjFl")
+//     .then(response => {
+//       const newData = response.data;
+//       const dataList = newData.response;
+//       const newList = [];
+//       dataList.forEach(item => {
+//         if (item.competition === "ENGLAND: Premier League" ||
+//           item.competiton === "ITALY: Serie A" ||
+//           item.competition === "GERMANY: Bundesliga") {
+//           newList.push(item);
+//         }
+//       })
+//       const randomChoice = newList[Math.floor(Math.random() * newList.length)];
+//       const newVideo = String(randomChoice.videos[0].embed);
+//       const highlight = new Highlight({
+//         video: newVideo,
+//         date: getDate()
+//       });
+//       highlight.save();
+//     });
+// }, null, true, 'UTC');
+
 
 const newSaying = () => {
   https.get("https://api.quotable.io/random", function(response) {
@@ -178,8 +274,7 @@ mongoose.connect("mongodb+srv://admin-dillon:dailydose@cluster0.szdh0.mongodb.ne
   if (error) {
     console.log(error);
   } else {
+    console.log("Connected");
     getDailyData();
   }
-})
-// .then(console.log("Connected to database."))
-// .then(newPoem());
+});
