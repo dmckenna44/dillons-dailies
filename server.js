453d05c4ab314a328d6f722147f37f230f3383c4
Dillon McKenna <dillonmckenna@Dillons-MacBook-Pro.local>
2022-11-01 14:33:36 -0700

    ACTUALLY reupdate poem route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
 require('dotenv').config();
-const cors =  require('cors');
+// const cors =  require('cors');
 const app = express();
 
 
